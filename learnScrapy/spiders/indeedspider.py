6c393242d0578cc1d80f89c3755f818891e255bf
Jeremy <kreppsjs@gmail.com>
2016-01-12 21:15:28 -0500

    Genesis commit

---
+++ learnScrapy/spiders/indeedspider.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import scrapy
+#from scrapy.spider import BaseSpider
+from scrapy.selector import Selector
+from learnScrapy.items import LearnscrapyItem
+#from scrapy.shell import inspect_response
+
+class IndeedSpider(scrapy.Spider):
+    name = "indeedspider"
+    allowed_domains = ["www.indeed.com"]
+    start_urls = [
+        'http://www.indeed.com/jobs?q=mechanical+engineer&l=united+states',
+    ]
+
+    def parse(self, response):
+        sel = Selector(response)
+#                       can also apparently do response.xpath() directly
+        rows = sel.xpath('//div[@class="  row  result"]')
+        items = []
+
+        for row in rows:
+            item = LearnscrapyItem()
+            item['jobTitle'] = row.xpath('h2[@class="jobtitle"]/a/@title').extract()
+            items.append(item)
+
+        return items
