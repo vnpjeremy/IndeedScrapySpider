c74e1250162c6d332806fad2dae76ec9339834d6
Jeremy <kreppsjs@gmail.com>
2016-01-17 15:04:22 -0500

    Get the sponsored links for loop nest referencing the right variable

--- learnScrapy/items.py
+++ learnScrapy/items.py
@@ -14,7 +14,6 @@ class LearnscrapyItem(Item):
     # name = scrapy.Field()
     jobTitle = Field()
     company = Field()
-    company2 = Field()
     # linkURL = Field()
     # location = Field()
 
--- learnScrapy/spiders/indeedspider.py
+++ learnScrapy/spiders/indeedspider.py
@@ -15,37 +15,41 @@ class IndeedSpider(scrapy.Spider):
     def parse(self, response):
         sel = Selector(response)
 #                       can also apparently do response.xpath() directly
-        rows = sel.xpath('//div[@class="  row  result"]')
+      #  rows = sel.xpath('//div[@class="  row  result"]')
         sponsored = sel.xpath('//div[@data-tn-section="sponsoredJobs"]')
-        lastRow = sel.xpath('//div[@class="lastRow  row  result"]')
-        lastSponsoredRow = sel.xpath('//div[@class="row sjlast result"]')
+       # lastRow = sel.xpath('//div[@class="lastRow  row  result"]')
+      #  lastSponsoredRow = sel.xpath('//div[@class="row sjlast result"]')
 
         items = []
 
         """ Gather the Job listings under 'sponsored """
         for spons in sponsored:
-            item = LearnscrapyItem()
-            item['jobTitle'] = spons.xpath('div/a/@title').extract()[0]
-            item['company'] = spons.xpath('div/div/span[@class="company"]/text()').extract()[0].strip('\n\t')
-            items.append(item)
+            subRows = spons.xpath('div')
+            for sub in subRows:
+                item = LearnscrapyItem()
+                item['jobTitle'] = sub.xpath('a/@title').extract()[0]
+                item['company'] = sub.xpath('div/span[@class="company"]/text()')\
+                    .extract()[0].strip('\n\t ')  # seems [0] is necessary. ?
+                items.append(item)
 
         """ Get the last sponsored job listing """
        # item1 = LearnscrapyItem()
        # item1['jobTitle'] = lastSponsoredRow.xpath('div/a/@title').extract()[0]
-        # item1['company'] = lastSponsoredRow.xpath('div/div/span[@class="company"]/text()').extract()[0].strip('\n\t')
-        # items.append(item1)
+       # item1['company'] = lastSponsoredRow.xpath('div/div/span[@class="company"]/text()').extract()[0].strip('\n\t')
+       # items.append(item1)
 
         """ Gather normal job listings, minus the last 'special' one """
-        for row in rows:
-            item = LearnscrapyItem()
-            item['jobTitle'] = row.xpath('h2/a/@title').extract()[0]
-            item['company'] = row.xpath('span/span[@itemprop="name"]/text()').extract()[0].strip('\n\t').lstrip()
-            items.append(item)
+       # for row in rows:
+          #  item = LearnscrapyItem()
+           # item['jobTitle'] = row.xpath('h2/a/@title').extract()[0]
+          # try strip('\n\t ') instead of two strip calls. Maybe work better?
+           # item['company'] = row.xpath('span/span[@itemprop="name"]/text()').extract()[0].strip('\n\t').lstrip()
+            # items.append(item)
 
         """ Get the last normal job listing """
-        item = LearnscrapyItem()
-        item['jobTitle'] = lastRow.xpath('h2/a/@title').extract()[0]
-        item['company'] = row.xpath('span/span[@itemprop="name"]/text()').extract()[0].strip('\n\t').lstrip()
-        items.append(item)
+       # item = LearnscrapyItem()
+      #  item['jobTitle'] = lastRow.xpath('h2/a/@title').extract()[0]
+       # item['company'] = row.xpath('span/span[@itemprop="name"]/text()').extract()[0].strip('\n\t').lstrip()
+        # items.append(item)
 
         return items
