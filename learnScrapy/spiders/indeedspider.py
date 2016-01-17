095ab21246cb5291665b80532a270dd12e32dbc9
Jeremy <kreppsjs@gmail.com>
2016-01-17 16:55:10 -0500

    Python likes a whitespace character at the end of the line, it seems. Fixed the weird comment-deletion-breaks-stuff problem. Now some of the locations work.

--- learnScrapy/spiders/indeedspider.py
+++ learnScrapy/spiders/indeedspider.py
@@ -14,13 +14,12 @@ class IndeedSpider(scrapy.Spider):
 
     def parse(self, response):
         sel = Selector(response)
-#                       can also apparently do response.xpath() directly
-        rows = sel.xpath('//div[@class="  row  result"]')
+        rows = sel.xpath('//div[@class="  row  result"]')  # can also apparently do response.xpath() directly
         sponsored = sel.xpath('//div[@data-tn-section="sponsoredJobs"]')
         lastRow = sel.xpath('//div[@class="lastRow  row  result"]')
-        items = []
 
         """ Gather the Job listings under 'sponsored """
+        items = []
         for spons in sponsored:
             subRows = spons.xpath('div')
             for sub in subRows:
@@ -28,14 +27,13 @@ class IndeedSpider(scrapy.Spider):
                 item['jobTitle'] = sub.xpath('a/@title').extract()[0]
                 item['company'] = sub.xpath('div/span[@class="company"]/text()')\
                     .extract()[0].strip('\n\t ')  # seems [0] is necessary. ?
-               # item['location'] = sub.xpath('div/span[@class="location"]/text()').extract()[0]
+                item['location'] = sub.xpath('div/span[@class="location"]/text()').extract()[0]
                 items.append(item)
 
         """ Gather normal job listings, minus the last 'special' one """
         for row in rows:
             item = LearnscrapyItem()
             item['jobTitle'] = row.xpath('h2/a/@title').extract()[0]
-          # try strip('\n\t ') instead of two strip calls. Maybe work better?
             item['company'] = row.xpath('span/span[@itemprop="name"]/text()') \
                 .extract()[0].strip('\n\t ')
             items.append(item)
