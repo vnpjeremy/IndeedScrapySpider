b8921a68f229e792c2cac2c642b3fca83d8e623c
Jeremy <kreppsjs@gmail.com>
2016-02-04 21:12:50 -0500

    lstrip handles the company field a lot better. Careful using the strip operator.

--- learnScrapy/spiders/indeedspider.py
+++ learnScrapy/spiders/indeedspider.py
@@ -9,8 +9,12 @@ class IndeedSpider(scrapy.Spider):
     name = "indeedspider"
     allowed_domains = ["www.indeed.com"]
     start_urls = [
-        'http://www.indeed.com/jobs?q=mechanical+engineer&l=united+states&sort=date',
+        # 'http://www.indeed.com/jobs?q=mechanical+engineer&l=united+states&sort=date',
+        'http://www.indeed.com/jobs?q=title%3A%28energy+engineer%29&l=united+states',
     ]
+    # base_url = "http://www.indeed.com/jobs?q=mechanical+engineer&l=united+states&sort=date&start="
+    # for i in range(10, 110, 10):
+    #     start_urls.append(base_url + str(i))
 
     def parse(self, response):
         sel = Selector(response)
@@ -26,7 +30,7 @@ class IndeedSpider(scrapy.Spider):
                 item = LearnscrapyItem()
                 item['jobtitle'] = sub.xpath('a/@title').extract()[0]
                 item['company'] = sub.xpath('div/span[@class="company"]'
-                                            '/text()').extract()[0].strip('\n\t ')
+                                            '/text()').extract()[0].lstrip('\n\t ')
                 item['location'] = sub.xpath('div/span[@class="location"]'
                                              '/text()').extract()[0]
                 item['date'] = sub.xpath('div/div/span[@class="date"]'
@@ -38,7 +42,7 @@ class IndeedSpider(scrapy.Spider):
             item = LearnscrapyItem()
             item['jobtitle'] = row.xpath('h2/a/@title').extract()[0]
             item['company'] = row.xpath('span/span[@itemprop="name"]'
-                                        '/text()').extract()[0].strip('\n\t ')
+                                        '/text()').extract()[0].lstrip('\n\t ')
             item['location'] = row.xpath('span/span/span[@itemprop="addressLocality"]'
                                          '/text()').extract()[0].strip('\n\t ')
             item['date'] = row.xpath('table/tr/td/div/div/span[@class="date"]'
@@ -49,7 +53,7 @@ class IndeedSpider(scrapy.Spider):
         item = LearnscrapyItem()
         item['jobtitle'] = lastrow.xpath('h2/a/@title').extract()[0]
         item['company'] = lastrow.xpath('span/span[@itemprop="name"]'
-                                        '/text()').extract()[0].strip('\n\t ')
+                                        '/text()').extract()[0].lstrip('\n\t ')
         item['location'] = lastrow.xpath('span/span/span[@itemprop="addressLocality"]'
                                          '/text()').extract()[0].strip('\n\t ')
         item['date'] = lastrow.xpath('table/tr/td/div/div/span[@class="date"]'
