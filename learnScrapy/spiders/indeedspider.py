59f987b778170cd5c91fd5925afa96763f364f09
Jeremy <kreppsjs@gmail.com>
2016-02-17 21:02:19 -0500

    callback is working! need to figure out textualization and search now

--- learnScrapy/spiders/indeedspider.py
+++ learnScrapy/spiders/indeedspider.py
@@ -3,6 +3,7 @@ import scrapy
 
 from scrapy.selector import Selector
 from learnScrapy.items import LearnScrapyItem
+from scrapy.http import Request
 
 
 class IndeedSpider(scrapy.Spider):
@@ -13,14 +14,14 @@ class IndeedSpider(scrapy.Spider):
     ]
     base_url = "http://www.indeed.com/jobs?q=mechanical+title%3Aengineer&sort=date&sr=directhire&start="
 
-    for i in range(10, 100, 10):
+    for i in range(10, 10, 10):
         start_urls.append(base_url + str(i))
 
     def parse(self, response):
         sel = Selector(response)
         rows = sel.xpath('//div[@class="  row  result" or @class="lastRow  row  result"]')
         sponsored = sel.xpath('//div[@data-tn-section="sponsoredJobs"]')
-
+        url_prefix = "http://www.indeed.com"
         items = []
         """ Sponsored job listings have a different footprint """
         for ii in sponsored:
@@ -42,6 +43,18 @@ class IndeedSpider(scrapy.Spider):
             item['location'] = ii.xpath('span/span/span[@itemprop="addressLocality"]/text()').extract()
             item['date'] = ii.xpath('table/tr/td/div/div/span[@class="date"]/text()').extract()
             item['link_url'] = ii.xpath('h2/a/@href').extract()
+
+            url = url_prefix + item['link_url'][0]
+
+            request = Request(url, callback=self.parse_job_link)
+            request.meta['item'] = item
+            yield request
             items.append(item)
 
-        return items
+        return
+
+    def parse_job_link(self, response):
+        sel = Selector(response)
+        item = response.request.meta['item']
+
+        return item
